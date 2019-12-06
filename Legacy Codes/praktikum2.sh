778310f4bfa8dc967a70f650143aec107e4290a0
alfarezaos <58576669+alfarezaos@users.noreply.github.com>
2019-12-06 14:35:06 +0700

    Create praktikum2.sh

---
+++ Legacy Codes/praktikum2.sh
@@ -0,0 +1,18 @@
+echo -n "masukkan detikknya :";
+read detik;
+
+if [ $detik -ge 3600 ]
+then
+let jam=$detik/3600;
+let detik=$detik-$jam*3600;
+echo "$jam jam"
+fi
+
+if [ $detik -ge 60 ]
+then
+let menit=$detik/60;
+let detik=$detik-$menit*60;
+echo "$menit menit"
+fi
+
+echo "$detik detik";
